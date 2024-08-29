2be2ea4f05ff55600f3a98f92e4b61537a80fdb4
Aravind Ranga <aravind.ranga@gmail.com>
2024-08-29 23:15:14 +0530

    variables

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh how are you "
+echo "Suresh :: Hello Ramesh"
+echo "Ramesh :: I am doing good"
+echo "Suresh ::  I am learning shell script"
+echo "Iam in  inaid "
+echo " I am father for karthikeya"
+
